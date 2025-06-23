169ca4a5cb29c1d4b6a8d097466fd7f037be857a
Muhammad Ayan <ayan.muhammad2912@gmail.com>
2025-06-23 03:08:06 -0700

    numbers: prints numbers from 1-10 using for loop and printf

---
+++ Loops/numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_1_to_10()
+{
+    for (int i = 1; i <= 10; i++) /*Prints a number and then 
+                                    increments and prints the next number*/
+    {
+        printf("number %d\n", i);  
+    }
+}
+
+
